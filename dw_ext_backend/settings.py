59b69b2caf052073ef4a3944e6da723388a9555d
egamboat <egamboat@unemi.edu.ec>
2025-02-08 17:47:21 -0500

    Implementación Reset Password

--- dw_ext_backend/settings.py
+++ dw_ext_backend/settings.py
@@ -146,3 +146,13 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
 }
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD =os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+FRONT_URL = os.getenv('FRONT_ENV_URL')
--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,9 @@
+from django.utils.timezone import now
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
+
+from users.models import CustomUser
 
 User = get_user_model()
 
@@ -14,3 +17,37 @@ class UserSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(**validated_data)
         Token.objects.create(user=user)
         return user
+
+class PasswordResetRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+    def validate_email(self, value):
+
+        if not User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("No se encontró una cuenta con este email.")
+        return value
+
+class PasswordResetSerializer(serializers.Serializer):
+    token = serializers.CharField()
+    new_password = serializers.CharField(min_length=8, write_only=True)
+
+    def validate_token(self, value):
+        try:
+            user = CustomUser.objects.get(reset_token=value)
+        except CustomUser.DoesNotExist:
+            raise serializers.ValidationError("Token inválido.")
+
+        if user.reset_token_expires_at and user.reset_token_expires_at < now():
+            raise serializers.ValidationError("El token ha expirado.")
+
+        return value
+
+    def save(self):
+        token = self.validated_data['token']
+        new_password = self.validated_data['new_password']
+
+        user = CustomUser.objects.get(reset_token=token)
+        user.set_password(new_password)
+        user.reset_token = None
+        user.reset_token_expires_at = None
+        user.save()
